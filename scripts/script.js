a6269c5a1cf863f7a08e5976a5b8e748e75b3b33
AleksNau <workciti@yandex.ru>
2022-09-01 21:15:06 +0300

    1 задание

--- scripts/script.js
+++ scripts/script.js
@@ -2,5 +2,7 @@ const productName = "Вареники";
 const productAmount = 3;
 const productCategory = "Продукты";
 const productPrice = 300;
+const productSumPrice = productAmount * productPrice;
 
 console.log("Наименование товара: ",productName )
+console.log("Общую сумма товаров: ",productSumPrice,"р")
