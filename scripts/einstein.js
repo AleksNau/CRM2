965f11394ae570a1d00ee87b7e9f735727229bca
AleksNau <workciti@yandex.ru>
2022-09-03 11:29:06 +0300

    2е задание

---
+++ scripts/einstein.js
@@ -0,0 +1,3 @@
+const myWeight = 74 ;
+const speedLight = 3e8 ;
+console.log(" E=mc2: ",  myWeight * (speedLight ** 2) );
--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
-const productName = "Вареники";
-const productAmount = 3;
-const productCategory = "Продукты";
-const productPrice = 300;
+const productName =  prompt('Наименование товара:');
+const productAmount = +(prompt('Количество товара:'));
+const productCategory = prompt('Категория товара:');
+const productPrice = +(prompt('Цена товара:'));
 const productSumPrice = productAmount * productPrice;
 
-console.log("Наименование товара: ",productName )
-console.log("Общую сумма товаров: ",productSumPrice + "р")
+console.log(`Наименование товара: ${productName}`);
+console.log(`Общая сумма товаров: ${productSumPrice}р`);
+console.log(typeof productPrice);
+console.log(`На складе ${productAmount} единиц(ы) товара "${productName}" на сумму ${productSumPrice} деревянных`)
