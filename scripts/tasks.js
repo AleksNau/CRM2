b4dae3c645bcb105c88f53f8f253242c18fc0623
AleksNau <workciti@yandex.ru>
2022-09-04 13:52:24 +0300

    задания 1-3

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -14,5 +14,12 @@ const history = (prompt('История:'));
 const english = prompt('Английский язык:');
 
 if ((math + history + english)>=265){
-    console.log('Поздравляю, вы поступили на бюджет!')
+    console.log('Поздравляю, вы поступили на бюджет!');
+}
+
+const money = prompt('Какую сумму желаете снять?');
+if (money % 100 === 0){
+    console.log('Получите ваши денежки');
+} else {
+    console.log('В банкомате отсутствуют нужные купюры');
 }
