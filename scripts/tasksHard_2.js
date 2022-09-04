a3652db2475c8e19ab672daa8ebe0ea6f4a4ecff
AleksNau <workciti@yandex.ru>
2022-09-05 00:44:22 +0300

    задания

--- scripts/tasksHard_2.js
+++ scripts/tasksHard_2.js
@@ -1,8 +1,8 @@
-const income2 =  prompt('какой у вас доход');
-if (income2>50000) {
-    console.log('Ваша ставка 0.3. Сумма налога:',(income2-50000) * 0.3 + 35000 * 0.2 + 15000 * 0.13);
-} else if(income2>=15000 && income2<=50000) {
-    console.log('Ваша ставка 0.2. Сумма налога:',(income2-15000) * 0.2 + (15000 * 0.13));
+const incomePro =  prompt('Какой у вас доход');
+if (incomePro>50000) {
+    console.log('Ваша ставка 0.3. Сумма налога:',(incomePro-50000) * 0.3 + 35000 * 0.2 + 15000 * 0.13);
+} else if(incomePro>=15000 && incomePro<=50000) {
+    console.log('Ваша ставка 0.2. Сумма налога:',(incomePro-15000) * 0.2 + (15000 * 0.13));
 } else {
-    console.log('Ваша ставка 0.1. Сумма налога:',income2 * 0.13);
+    console.log('Ваша ставка 0.1. Сумма налога:',incomePro * 0.13);
 }
