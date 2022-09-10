91fddd1e14bc8fee4fcc2facb02874b5be7e1afa
AleksNau <workciti@yandex.ru>
2022-09-10 19:13:47 +0300

    задания

--- scripts/tasksHard_1.js
+++ scripts/tasksHard_1.js
@@ -1,10 +1,13 @@
 const income = prompt('Какой у вас доход?');
-if (income > 50000) {
-    console.log('Ваша ставка 0.3. Сумма налога:',income * 0.3);
-} else if (income >= 15000 && income <= 50000) {
-    console.log('Ваша ставка 0.2. Сумма налога:',income * 0.2);
-} else if (income < 0) {
-    console.log('Вы ввели некоректные данные');
+
+if (income > 0) {
+    if (income > 50000) {
+        console.log('Ваша ставка 0.3. Сумма налога:',income * 0.3);
+    } else if (income >= 15000 && income <= 50000) {
+        console.log('Ваша ставка 0.2. Сумма налога:',income * 0.2);
+    } else {
+        console.log('Ваша ставка 0.1. Сумма налога:',income * 0.13);
+    }
 } else {
-    console.log('Ваша ставка 0.1. Сумма налога:',income * 0.13);
+    console.log('Вы ввели некоректные данные');
 }
--- scripts/tasksHard_2.js
+++ scripts/tasksHard_2.js
@@ -1,10 +1,14 @@
-const incomePro =  prompt('Какой у вас доход');
+const incomePro =  prompt('Какой у вас доход про');
+let tax = 0;
 if (incomePro > 50000) {
-    console.log('Ваша ставка 0.3. Сумма налога:',(incomePro-50000) * 0.3 + 35000 * 0.2 + 15000 * 0.13);
+    tax = (incomePro % 50000 * 0.3) + 35000 * 0.2 + 15000 * 0.13;
+    console.log(tax);
 } else if (incomePro >= 15000 && incomePro <= 50000) {
-    console.log('Ваша ставка 0.2. Сумма налога:',(incomePro-15000) * 0.2 + (15000 * 0.13));
+    tax = (incomePro - 15000) * 0.2 + 15000 * 0.13;
+    console.log(tax);
 } else if (income < 0) {
     console.log('Вы ввели некоректные данные');
 }  else {
-    console.log('Ваша ставка 0.1. Сумма налога:',incomePro * 0.13);
+    tax = incomePro * 0.13;
+    console.log(tax);
 }
