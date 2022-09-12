0ef75c5e939f1dc5c1d1af402d51e4696a3d3e78
AleksNau <workciti@yandex.ru>
2022-09-12 11:38:04 +0300

    5 урок

--- scripts/Task01.js
+++ scripts/Task01.js
@@ -1,13 +1,3 @@
-const convertToEuro = (money) => {
-    let rub = money;
-    rub *= 73;
-    return rub;
-}
-
-const money = convertToEuro(prompt('какую сумму хотите конвертировать?'));
-console.log(money);
-
-/*Выведите в консоль таблицу степеней от 1 до 10 по аналогии как в уроке выводится таблица умножения*/
 for (let i = 2;i < 10;i++){
     console.log('------------');
     for (let y = 1;y<10; y++){
--- scripts/Task03.js
+++ scripts/Task03.js
@@ -1,46 +1,9 @@
-const calculate = (sum = 0,mount = 0,promo = '') => {
-    let amount = mount;
-    let price = sum;
-    let code = promo;
-    if (amount > 10){
-        price = price - (price * 0.03);
-        if (price > 30000) {
-        price = price - ((price - 30000) * 0.15);
-            if (code === 'METHED'){
-            price = price - (price * 0.1);
-            return price;
-            }  else if (code === 'G3H2Z1'&& price > 2000) {
-            price = price - 500;
-            return price;
-            }
-        return price;
-        }
-    return price;
+let backString = (string) => {
+    let b = '';
+    for (a = string.length - 1 ;a >= 0; a--){
+        b += string[a];
     }
-    else if (price > 30000) {
-        price = price - ((price - 30000) * 0.15);
-        if (code === 'METHED'){
-            price = price - (price * 0.1);
-            return price;
-        }  else if (code === 'G3H2Z1' && price > 2000) {
-            price = price - 500;
-            return price;
-        }
-        return price;
-    } else if (code === 'METHED'){
-        price = price - (price * 0.1);
-        return price;
-    }  else if (code === 'G3H2Z1' && price > 2000) {
-        price = price - 500;
-        return price;
-    }
-
-    return price;
+    return b;
 }
-
-const finalPrice = calculate( prompt('сумма?'),prompt('количество товаров'),prompt('промокод'));
-console.log('Финальная стоимость:',  finalPrice);
-
-/*Напишите функцию, которая принимает строку и возвращает обратную строку
-
-Пример: "Привет мир" => "рим тевирП"*/
+const reverseString = backString(prompt('Введите строку'));
+console.log(reverseString);
