8cac4a89aba602d492c831ce854c40de5e16bee7
AleksNau <workciti@yandex.ru>
2022-09-13 16:53:05 +0300

    2/3 6 задания

--- scripts/Task01.js
+++ scripts/Task01.js
@@ -9,18 +9,8 @@
 Результат функции: массив из всех студентов, которые сдали экзамен.
 */
 
-const filter = (massive1, massive2) => {
-    let firstMassive = massive1;
-    let secondMassive = massive2;
-    let finalMassive = [];
-    for (let i=0;i<firstMassive.length;i++){
-        if (firstMassive.includes(secondMassive[i])){
-          finalMassive = firstMassive.delete(firstMassive[i]);
-        }
-    }
-return finalMassive;
-}
 const allStudents = ['Иванов','Петров','Сидоров','Кузнецов','Смирнов','Попов','Соколов'];
 const failedStudents = ['Сидоров','Смирнов','Попов'];
-console.log(filter(allStudents,failedStudents));
+const result = allStudents.filter(i => !failedStudents.includes(i));
+console.log(result);
 
--- scripts/Task02.js
+++ scripts/Task02.js
@@ -7,3 +7,16 @@
 
 const allCashbox = [4500, 3210, 650, 1250, 7830, 990, 13900, 370];
 */
+
+const allCashbox = [4500, 3210, 650, 1250, 7830, 990, 13900, 370];
+let a = 0;
+const getAverageValue = () => {
+    for (i = 0;i <allCashbox.length; i++){
+
+        a += allCashbox[i];
+    }
+    a = a / allCashbox.length;
+    return a;
+};
+const average = getAverageValue(allCashbox);
+console.log(average);
