77f27a22b05b08d8982074368bde2f37d318dbd5
AleksNau <workciti@yandex.ru>
2022-09-13 17:24:39 +0300

    lesson completed

--- scripts/Task01.js
+++ scripts/Task01.js
@@ -1,5 +1,12 @@
 const allStudents = ['Иванов','Петров','Сидоров','Кузнецов','Смирнов','Попов','Соколов'];
 const failedStudents = ['Сидоров','Смирнов','Попов'];
-const result = allStudents.filter(i => !failedStudents.includes(i));
+
+const whoFailed = (allStudents,failedStudents) => {
+    const students = allStudents;
+    const failed = failedStudents;
+    const result = students.filter(i => !failed.includes(i));
+    return result;
+};
+const result = whoFailed(allStudents,failedStudents);
 console.log(result);
 
