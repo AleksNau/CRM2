946fba1532ff05f00db4c390b27ca971f7378092
AleksNau <workciti@yandex.ru>
2022-09-14 10:40:02 +0300

    lesson completed - codestyle

--- scripts/Task01.js
+++ scripts/Task01.js
@@ -1,12 +1,13 @@
 const allStudents = ['Иванов','Петров','Сидоров','Кузнецов','Смирнов','Попов','Соколов'];
 const failedStudents = ['Сидоров','Смирнов','Попов'];
 
-const whoFailed = (allStudents,failedStudents) => {
+const whoFailed = (allStudents, failedStudents) => {
     const students = allStudents;
     const failed = failedStudents;
     const result = students.filter(i => !failed.includes(i));
     return result;
 };
-const result = whoFailed(allStudents,failedStudents);
+
+const result = whoFailed(allStudents, failedStudents);
 console.log(result);
 
--- scripts/Task03.js
+++ scripts/Task03.js
@@ -1,12 +1,15 @@
-const names= ['Noah', 'Liam', 'Mason', 'Jacob', 'Robot', 'William', 'Ethan', 'Michael', 'Alexander'];
+const names = ['Noah', 'Liam', 'Mason', 'Jacob', 'Robot', 'William', 'Ethan', 'Michael', 'Alexander'];
+
 const addPrefix = (massive,prefix) => {
     const officialName = massive;
     const preName = prefix;
-    for (i = 0;i <officialName.length; i++){
 
+    for (i = 0;i <officialName.length; i++) {
         officialName[i] = preName + " " + officialName[i];
     }
+
     return officialName;
-};
+}
+
 const fullName = addPrefix(names,"Mr");
 console.log(fullName);
