19d7c2cb8b17b3304a2c05eb2dd7f442bb1a9af7
AleksNau <workciti@yandex.ru>
2022-09-16 15:49:32 +0300

    lesson completed

---
+++ scripts/game01.js
@@ -0,0 +1,22 @@
+        const rightNumber = (number) => {
+        const value = Math.floor(Math.random() * 101);
+        const a = number;
+
+        if (isNaN(a)) {
+            return console.log('Не число!')
+        }
+
+        if (value > a) {
+            return console.log('Больше!')
+        }
+
+        if (value === a) {
+            return console.log('Правильно!')
+        }
+
+        if (value < a) {
+            return console.log('Меньше!')
+        }
+    }
+
+rightNumber(prompt());
