6df7df135767ee9754ae8d5a3de2145ee36c6c86
AleksNau <workciti@yandex.ru>
2022-10-03 18:50:48 +0300

    3nd

--- scripts/Task03.js
+++ scripts/Task03.js
@@ -1,28 +1,19 @@
 const generate = (length, min, max, code) => Array(length).fill().map(() => {
+    if (max > 0) {
+        max -= 1;
+    }
     let item = Math.round((Math.random() * (max - min)) + min);
-    if (code === 'even') {
-        if (item % 2 !== 0 && item > 0) {
+        if (item % 2 !== 0  && code === 'even') {
             item += 1;
         }
-        if (item % 2 !== 0 && item < 0) {
-            item -= 1;
+        if (item % 2 === 0 && code === 'odd') {
+            item += 1;
         }
         if (item === -0 ) {
             item = Math.abs(item);
         }
-    }
-    if (code === 'odd') {
-        if (item % 2 === 0 && item > 0) {
-            item -= 1;
-        }
-        else if (item % 2 === 0 && item < 0) {
-            item += 1;
-        }
-        else if (item === -0 ) {
-            item = Math.abs(item);
-        }
-}
+
     return item;
 })
 
-console.log(generate(100,-10,10,`odd`),);
+console.log(generate(100,10,-10,`even`),);
