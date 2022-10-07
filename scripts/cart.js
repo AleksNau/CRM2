62a2dd6651ed74cc7f5f0cd25c38b98cbb2d9d17
AleksNau <workciti@yandex.ru>
2022-10-07 20:50:33 +0300

    не пойму

--- scripts/cart.js
+++ scripts/cart.js
@@ -8,27 +8,29 @@ const cart = {
     },
 
     add(name,price,amount = 1) {
-
-        if (this.items.includes(name)){
-            this.totalPrice += price * amount;
-            this.count +=amount;
-        } else {
-            this.items.push(name);
-            this.totalPrice += price * amount;
-            this.count +=amount;
-        }
-
-        return this.items,this.totalPrice,this.count;
+        const item = {};
+        item.name = name;
+        item.price = price;
+        item.count = amount;
+        return this.items.push(item);
 
     },
 
     increaseCount(amount) {
+        let itemCount = 0;
+        for (i = 0;i < this.items.length; i++) {
+            itemCount += this.items[i].count;
+        }
         this.count += amount;
-        return this.count;
+        return this.count =  itemCount;
     },
 
     calculateItemPrice() {
-        return this.totalPrice;
+        let itemPrice = 0;
+        for (i = 0;i < this.items.length; i++) {
+            itemPrice += this.items[i].price * this.items[i].count;
+        }
+        return this.totalPrice = itemPrice;
     },
 
     clear() {
@@ -47,4 +49,7 @@ cart.add(`подушки`,200,2);
 cart.add(`тарелки`,200,3);
 cart.add(`бельё`,450,1);
 cart.increaseCount(2);
+console.log(cart.calculateItemPrice())
+console.log(cart.print());
+cart.clear();
 console.log(cart.print());
