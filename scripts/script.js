597712c9859f720f709c1d69148e9ecfd9d8f917
AleksNau <workciti@yandex.ru>
2022-10-13 12:32:36 +0300

    Completed

--- scripts/script.js
+++ scripts/script.js
@@ -2,12 +2,12 @@ const randomMassive = [];
 
 function randomZeroTen(massive) {
     const number = Math.round(Math.random() * 10);
-    const rid = massive;
-    rid.push(number);
-    if ((rid.reduce((a, b) => a + b, 0) < 50)){
-        return randomZeroTen(rid);
+    const randomNumbers = massive;
+    randomNumbers.push(number);
+    if ((randomNumbers.reduce((a, b) => a + b, 0) < 50)){
+        return randomZeroTen(randomNumbers);
     }
-    return rid;
+    return randomNumbers;
 }
 
 console.log(randomZeroTen(randomMassive));
