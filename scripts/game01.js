362c6bbc8a6f3a17b615b014f6a0fff80b7f2458
AleksNau <workciti@yandex.ru>
2022-10-16 18:33:18 +0300

    Completed lint

--- scripts/game01.js
+++ scripts/game01.js
@@ -1,19 +1,20 @@
+'use strict'
 const value = Math.ceil(Math.random() * 100);
 
 function rightNumber(randomNumber) {
-        const userInput = prompt('Введите число');
-        const userNumber = +userInput;
-        if (userInput != NaN){
-            if (value < userNumber) {
-                alert('Меньше!');
-                return rightNumber();
-            } else if (value > userNumber) {
-                alert('Больше!');
-                return rightNumber();
-            } else if (userNumber === value) {
-               return alert('Правильно!');
-            }
-        }
+  const userInput = prompt('Введите число');
+  const userNumber = +userInput;
+  if (userInput != NaN) {
+    if (value < userNumber) {
+      alert('Меньше!');
+      return rightNumber();
+    } else if (value > userNumber) {
+      alert('Больше!');
+      return rightNumber();
+    } else if (userNumber === value) {
+      return alert('Правильно!');
+    }
+  }
 }
 
 rightNumber();
--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,14 @@
+'use strict'
 const randomMassive = [];
 
 function randomZeroTen(massive) {
-    const number = Math.round(Math.random() * 10);
-    const randomNumbers = massive;
-    randomNumbers.push(number);
-    if ((randomNumbers.reduce((a, b) => a + b, 0) < 50)){
-        return randomZeroTen(randomNumbers);
-    }
-    return randomNumbers;
+  const number = Math.round(Math.random() * 10);
+  const randomNumbers = massive;
+  randomNumbers.push(number);
+  if ((randomNumbers.reduce((a, b) => a + b, 0) < 50)) {
+    return randomZeroTen(randomNumbers);
+  }
+  return randomNumbers;
 }
 
 console.log(randomZeroTen(randomMassive));
